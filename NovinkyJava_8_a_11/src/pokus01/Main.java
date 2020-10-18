a59a8f7e472d44cd8fd00d7f60315f6af2010da0
jasla <jasla@LAPTOP-20AA7S94>
2020-10-18 10:03:20 +0200

    Prvni commit z eclipse

---
+++ NovinkyJava_8_a_11/src/pokus01/Main.java
@@ -0,0 +1,37 @@
+package pokus01;
+
+/*kuk*/
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list = Arrays.asList("a","z","b","x","c","d","jouda","mòouk","mrsk");
+
+		list.stream().forEach(System.out::println);
+		System.out.println("--01---------------------------------------");
+		
+		list = list.stream()
+				   .sorted()
+				   .map(x -> x+x)
+				   .filter(x -> !x.equals("bb"))
+				   .map(String::toUpperCase)
+				   .filter(x -> !x.equals("XX") && !x.equals("AA"))
+				   .map(x -> x+"_"+x.length())
+				   .collect(Collectors.toList());
+        System.out.println(list);
+        System.out.println("--02---------------------------------------");
+
+        list.stream().forEach(System.out::println);
+        System.out.println("--03---------------------------------------");
+	}
+
+}
